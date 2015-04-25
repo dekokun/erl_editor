2bd8b84361f2f806e705145cfbeffaf385d8a33c
dekokun <shintaro.kurachi@gmail.com>
2015-04-25 13:19:56 +0900

    first commit

---
+++ src/message_wall_app.erl
@@ -0,0 +1,26 @@
+-module(message_wall_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+  ets:new(message_wall, [ordered_set, named_table, public]),
+  Dispatch = cowboy_router:compile([
+    {'_', [
+      % cowboy_staticはパスマッチに対して、静的ファイルを読み込む
+      % index.htmlを読み込む
+      {"/", cowboy_static, {priv_file, message_wall, "index.html"}},
+      % /websocketのリクエストをws_handlerに渡す
+      {"/websocket", message_wall_handler, []}
+    ]}
+  ]),
+  {ok, _} = cowboy:start_http(http, 100,
+    [{port, 8001}],
+    [
+      {env, [{dispatch, Dispatch}]}
+    ]),
+	message_wall_sup:start_link().
+
+stop(_State) ->
+	ok.
