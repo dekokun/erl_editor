50c0765608674ac9acb1a48e50427b8f0cf46980
dekokun <shintaro.kurachi@gmail.com>
2015-04-30 09:13:51 +0900

    privの場所に頼らない構成にした

--- src/message_wall_app.erl
+++ src/message_wall_app.erl
@@ -10,7 +10,9 @@ start(_Type, _Args) ->
     {'_', [
       % cowboy_staticはパスマッチに対して、静的ファイルを読み込む
       % index.htmlを読み込む
-      {"/", cowboy_static, {priv_file, message_wall, "index.html"}},
+      {"/", cowboy_static, {file, filename:join(
+        [filename:dirname(code:which(?MODULE)),
+          "..", "priv", "index.html"])}},
       % /websocketのリクエストをws_handlerに渡す
       {"/websocket", message_wall_handler, []}
     ]}
